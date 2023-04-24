e44be6ed4b2495d33cb9fe0cef47a598fe84b308
Beyhan <btstructure97@gmail.com>
2023-04-24 16:48:28 -0400

    made separate instances

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const jsonServer = require("json-server")
-const server = jsonServer.create()
-const router = jsonServer.router("mbti-personality.json")
-const router2 = jsonServer.router("questions.json")
+
+
+const server1 = jsonServer.create()
+const router1 = jsonServer.router("mbti-personality.json")
 const middlewares = jsonServer.defaults()
-const port = process.env.PORT || 3001
+const port1 = process.env.PORT || 3001
+
+server1.use(middlewares)
+server1.use(router1)
+server1.listen(port1, () => {
+    console.log(`Server is running on port ${port1}`)
+})
+
+
+const server2 = jsonServer.create()
+const router2 = jsonServer.router("questions.json")
+const middlewares2 = jsonServer.defaults()
 const port2 = process.env.PORT || 3002
 
-server.use(middlewares)
-server.use(router)
-server.use(router2)
-server.listen(port)
-server.listen(port2)
+
+
+server2.use(middlewares2)
+server2.use(router2)
+server2.listen(port2, () => {
+    console.log(`Server is running on port ${port2}`)
+})
