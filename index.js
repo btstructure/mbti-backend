08d624b1b7df8ddd0127ce806baa0ecb5c69d21b
Beyhan <btstructure97@gmail.com>
2023-06-03 19:09:24 -0400

    only personaltiies run

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router1 = jsonServer.router('mbti-personality.json');
-const router2 = jsonServer.router('questions.json');
+const jsonServer = require("json-server");
+const server1 = jsonServer.create();
+const server2 = jsonServer.create();
+const router1 = jsonServer.router("mbti-personality.json");
+const router2 = jsonServer.router("questions.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
+const port1 = process.env.PORT_1 || 3001;
+const port2 = process.env.PORT_2 || 3002;
 
-server.use(middlewares);
-server.use('/personality', router1);
-server.use('/questions', router2);
+server1.use(middlewares);
+server1.use(router1);
+server1.listen(port1, () => {
+  console.log(`Server 1 is running on port ${port1}`);
+});
 
-server.listen(port, () => {
-  console.log(`JSON Server is running on http://localhost:${port}`);
+server2.use(middlewares);
+server2.use(router2);
+server2.listen(port2, () => {
+  console.log(`Server 2 is running on port ${port2}`);
 });
