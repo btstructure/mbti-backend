1e0fc9520f9882bdf3b1f0463f87650b0e8f2c3e
Beyhan <btstructure97@gmail.com>
2023-06-03 18:47:20 -0400

    reverting back

--- index.js
+++ index.js
@@ -7,32 +7,14 @@ const middlewares = jsonServer.defaults();
 const port1 = process.env.PORT_1 || 3001;
 const port2 = process.env.PORT_2 || 3002;
 
-// Server 1
-server1.use("/personality", router1); // Use "/personality" as the base URL for router1
 server1.use(middlewares);
+server1.use(router1);
+server1.listen(port1, () => {
+  console.log(`Server 1 is running on port ${port1}`);
+});
 
-// Server 2
-server2.use("/questions", router2); // Use "/questions" as the base URL for router2
 server2.use(middlewares);
-
-// Start both servers
-Promise.all([
-  new Promise((resolve, reject) => {
-    server1.listen(port1, () => {
-      console.log(`Server 1 is running on port ${port1}`);
-      resolve();
-    });
-  }),
-  new Promise((resolve, reject) => {
-    server2.listen(port2, () => {
-      console.log(`Server 2 is running on port ${port2}`);
-      resolve();
-    });
-  })
-])
-  .then(() => {
-    console.log("Both servers are running.");
-  })
-  .catch((error) => {
-    console.error("Error starting servers:", error);
-  });
+server2.use(router2);
+server2.listen(port2, () => {
+  console.log(`Server 2 is running on port ${port2}`);
+});
